4263cbf503403ac60f3c4d22c7b1637e40d56543
Davide Masserut <d.masserut@gmail.com>
2018-01-03 21:26:01 +0100

    (.emacs.el): Customize diff-hl faces

--- .emacs.el
+++ .emacs.el
@@ -500,7 +500,12 @@
   (diff-hl-flydiff-mode)
   (diff-hl-margin-mode)
   (setq diff-hl-draw-borders nil
-        diff-hl-side 'right))
+        diff-hl-side 'right)
+  :custom-face
+  (diff-hl-margin-change ((t (:inherit diff-hl-change :foreground "black" :slant normal))))
+  (diff-hl-margin-delete ((t (:inherit diff-hl-delete :foreground "black"))))
+  (diff-hl-margin-ignored ((t (:inherit dired-ignored :foreground "black"))))
+  (diff-hl-margin-insert ((t (:inherit diff-hl-insert :foreground "black")))))
 
 ;; gitignore
 ;; https://github.com/syohex/emacs-gitignore
