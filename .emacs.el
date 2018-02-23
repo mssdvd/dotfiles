2283fc364db7ec839dfcc43e186be0e7c5f995e6
Davide Masserut <d.masserut@gmail.com>
2018-02-23 15:08:54 +0100

    Revert "(.emacs.el): Keep default projectile-keymap-prefix for compati"
    This reverts commit 5986783dd63c1518b018ed9e542235ae0e0c649c.

--- .emacs.el
+++ .emacs.el
@@ -475,7 +475,7 @@ Repeated invocations toggle between the two most recently open buffers."
 (use-package projectile
   :delight '(:eval (concat " " (projectile-project-name)))
   :defer 1
-  :bind-keymap ([menu] . projectile-command-map)
+  :init (setq projectile-keymap-prefix (kbd "<menu>"))
   :config
   (projectile-mode)
   (add-to-list 'projectile-project-root-files "platformio.ini")
