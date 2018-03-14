5b051564dcde9941bb1729b4e31f5b1996b62504
Davide Masserut <d.masserut@gmail.com>
2018-03-14 22:12:29 +0100

    (.emacs.el): Add org repository

--- .emacs.el
+++ .emacs.el
@@ -9,6 +9,8 @@
 (require 'package)
 (add-to-list 'package-archives
 			 '("MELPA" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+			 '("org" . "https://orgmode.org/elpa/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
