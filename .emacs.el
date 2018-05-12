8c078442c04eeeb5beb713ccf3c4be330074ef5d
Davide Masserut <d.masserut@gmail.com>
2018-05-12 18:45:19 +0200

    (.emacs.el): Don't add a title slide in org reveal

--- .emacs.el
+++ .emacs.el
@@ -428,7 +428,9 @@ Repeated invocations toggle between the two most recently open buffers."
 ;; https://github.com/yjwen/org-reveal
 (use-package ox-reveal
   :defer t
-  :config (setq org-reveal-root "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/"))
+  :config
+  (setq org-reveal-root "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/"
+        org-reveal-title-slide nil))
 
 ;; paradox
 (use-package paradox
