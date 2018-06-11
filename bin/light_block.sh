31f5149dec0d00a048c3e9d99f211a966d771611
Davide Masserut <d.masserut@gmail.com>
2018-06-11 18:16:12 +0200

    Add light block

---
+++ bin/light_block.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+case "$BLOCK_BUTTON" in
+    1) light -A 5 ;;
+    2) light -S 100 ;;
+    3) light -U 5 ;;
+    4) light -A 5 ;;
+    5) light -U 5 ;;
+esac
+
+LIGHT=$(light)
+echo $(LC_ALL=C /usr/bin/printf "%.0f" "$LIGHT")%
