6831001658b0cf184a443a52255fcd8ba88e69fb
Davide Masserut <d.masserut@gmail.com>
2018-09-01 22:36:16 +0200

    (bin/wacom.sh): Update wacom.sh

--- bin/wacom.sh
+++ bin/wacom.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +1 "key ctrl" "key z"
-xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +3 "key ctrl" "key shift" "key z"
-xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +9 "key ctrl" "key +"
+xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +3 "key ctrl" "key z"
+xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +1 "key ctrl" "key shift" "key z"
+xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +9 "key ctrl" "key shift" "key ="
 xsetwacom --set "Wacom Intuos S 2 Pad pad" Button +8 "key ctrl" "key -"
