cedb5d023d808919a46069aec76b5f6b6d78930b
Davide Masserut <d.masserut@gmail.com>
2018-10-30 14:59:32 +0100

    (bin/run_livecd.sh): Enable networking

--- bin/run_livecd.sh
+++ bin/run_livecd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-exec qemu-system-x86_64 -enable-kvm -smp cores=4,threads=8 -m 3G -cpu host -usb -device usb-tablet -cdrom "$@"
+exec qemu-system-x86_64 -enable-kvm -smp cores=4,threads=8 -m 3G -nic user,model=virtio -cpu host -usb -device usb-tablet -cdrom "$@"
