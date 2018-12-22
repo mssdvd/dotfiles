20eb316a543e86f23e9d992e07b8f7afd92c2ad8
Davide Masserut <d.masserut@gmail.com>
2018-12-22 15:30:33 +0100

    (bin/live_livecd.sh): Improve performance

--- bin/run_livecd.sh
+++ bin/run_livecd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-exec qemu-system-x86_64 -enable-kvm -smp cores=4,threads=8 -m 3G -nic user,model=virtio -cpu host -usb -device usb-tablet -cdrom "$@"
+exec qemu-system-x86_64 -machine type=q35,accel=kvm -smp cores=4,threads=2 -m 3G -nic user,model=virtio -cpu host -vga virtio -soundhw hda -usb -device usb-tablet -display gtk,gl=on -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -cdrom "$@"
