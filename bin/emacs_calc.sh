13644e0fecefd345107f1b13fcf85f105ec3a664
Davide Masserut <dm@mssdvd.com>
2021-01-29 22:51:15 +0100

    bin/emacs_calc.sh: use sh instead of bash

--- bin/emacs_calc.sh
+++ bin/emacs_calc.sh
@@ -1,3 +1,2 @@
-#! /bin/bash
-
+#!/bin/sh
 emacs -Q -D -r -T "emacs calc" -f full-calc --eval="(progn (setq calc-group-digits t select-enable-primary t) (set-frame-font \"-BE5n-Iosevka-semibold-normal-normal-*-23-*-*-*-d-0-iso10646-1\"))"
