a80f959f415b26b7ac390931714bd92cd292009f
Davide Masserut <dm@mssdvd.com>
2021-05-09 23:58:29 +0200

    bin/emacs_calc.sh: update

--- bin/emacs_calc.sh
+++ bin/emacs_calc.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-emacs -Q -D -r -T "emacs calc" -f full-calc --eval="(progn (setq calc-group-digits t select-enable-primary t) (set-frame-font \"-BE5n-Iosevka-semibold-normal-normal-*-23-*-*-*-d-0-iso10646-1\"))"
+emacs -Q -D -r -T "emacs calc" -f full-calc --eval="(progn (setq
+                      calc-group-digits t select-enable-primary
+                      t)(set-face-attribute 'default nil :family
+                      \"Iosevka\" :weight 'semi-bold :height 181))"
