48a5474f1c4c44a5fb3c4d70d3db97726b1aad7d
Davide Masserut <dm@mssdvd.com>
2021-07-15 20:42:58 +0200

    bin/rename_workspace.sh: use cut instead of awk
    Why? Why not.

--- bin/rename_workspace.sh
+++ bin/rename_workspace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-workspace=$(swaymsg -t get_workspaces| jq -r '.[] | select(.focused == true) | .name')
-workspace_number=$(echo $workspace | awk '{print $1}')
+workspace=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused == true) | .name')
+workspace_number=$(echo $workspace | cut -d' ' -f1)
 
 swaymsg rename workspace $workspace to "${workspace_number} ${@}"
