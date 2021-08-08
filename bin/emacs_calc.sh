1ae22a338e086a9a0a81f1e2637f48c389a95339
Davide Masserut <dm@mssdvd.com>
2021-08-08 14:13:57 +0200

    emacs_calc.sh: update config
    - Use long command line arguments
    - Disable native-comp warnings
    - bind q to kill-emacs

--- bin/emacs_calc.sh
+++ bin/emacs_calc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-emacs -Q -D -r -T "emacs calc" -f full-calc --eval="(progn (setq
-                      calc-group-digits t select-enable-primary
-                      t)(set-face-attribute 'default nil :family
-                      \"Iosevka\" :weight 'semi-bold :height 181))"
+emacs --quick --basic-display --reverse-video --title="emacs calc" --funcall=full-calc \
+      --eval="(progn (setq calc-group-digits t
+                           native-comp-deferred-compilation nil
+                           native-comp-async-report-warnings-errors nil
+                           select-enable-primary t)
+                     (set-face-attribute 'default nil
+                                         :family \"Iosevka\"
+                                         :weight 'semi-bold
+                                         :height 180)
+                     (local-set-key (kbd \"q\") 'kill-emacs))"
