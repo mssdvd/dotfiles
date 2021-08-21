2a25c5f67bfadcd3ac2650cc137f5b0aaf633cbb
Davide Masserut <dm@mssdvd.com>
2021-08-21 14:55:02 +0200

    emacs_read_new_mail.sh: show oldest email first

--- bin/emacs_read_new_email.sh
+++ bin/emacs_read_new_email.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-emacsclient -c -a '' --eval "(progn (require 'notmuch)(notmuch-search \"tag:unread\"))"
+emacsclient -c -a '' --eval "(progn (require 'notmuch)(notmuch-search \"tag:unread\" t))"
