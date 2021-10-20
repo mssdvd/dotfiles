257a78ae3c85f58429098b856fe6d741b113fa6d
Davide Masserut <dm@mssdvd.com>
2021-10-20 18:48:32 +0200

    vm/windows: enable USB redirection with SPICE
    This commit adds three USB slots for redirection

--- vm/win.sh
+++ vm/win.sh
@@ -15,6 +15,13 @@ qemu-system-x86_64 \
     -device hda-output,audiodev=snd0 \
     -rtc base=localtime \
     -usb -device usb-tablet \
+    -device ich9-usb-ehci1,id=usb \
+    -device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on \
+    -device ich9-usb-uhci2,masterbus=usb.0,firstport=2 \
+    -device ich9-usb-uhci3,masterbus=usb.0,firstport=4 \
+    -chardev spicevmc,name=usbredir,id=usbredirchardev1 -device usb-redir,chardev=usbredirchardev1,id=usbredirdev1 \
+    -chardev spicevmc,name=usbredir,id=usbredirchardev2 -device usb-redir,chardev=usbredirchardev2,id=usbredirdev2 \
+    -chardev spicevmc,name=usbredir,id=usbredirchardev3 -device usb-redir,chardev=usbredirchardev3,id=usbredirdev3 \
     -vga qxl \
     -device virtio-serial-pci \
     -spice unix=on,addr=/tmp/vm_spice.socket,disable-ticketing=on \
