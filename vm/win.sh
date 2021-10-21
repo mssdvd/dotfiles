6711636ff00e4c2ce95808a72123dc6f12482b4c
Davide Masserut <dm@mssdvd.com>
2021-10-21 14:01:46 +0200

    vm/windows: add monitor socket

--- vm/win.sh
+++ vm/win.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 VM_DIR="$HOME/vm"
+SPICE_SOCKET=/tmp/win_spice.socket
+MONITOR_SOCKET=/tmp/win_monitor.socket
 
 qemu-system-x86_64 \
     -daemonize \
@@ -14,6 +16,7 @@ qemu-system-x86_64 \
     -device ich9-intel-hda \
     -device hda-output,audiodev=snd0 \
     -rtc base=localtime \
+    -monitor unix:$MONITOR_SOCKET,server,nowait \
     -usb -device usb-tablet \
     -device ich9-usb-ehci1,id=usb \
     -device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on \
@@ -24,7 +27,7 @@ qemu-system-x86_64 \
     -chardev spicevmc,name=usbredir,id=usbredirchardev3 -device usb-redir,chardev=usbredirchardev3,id=usbredirdev3 \
     -vga none -device qxl-vga,vgamem_mb=64 \
     -device virtio-serial-pci \
-    -spice unix=on,addr=/tmp/vm_spice.socket,disable-ticketing=on \
+    -spice unix=on,addr=$SPICE_SOCKET,disable-ticketing=on \
     -device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
     -chardev spicevmc,id=vdagent,name=vdagent \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
@@ -34,6 +37,5 @@ qemu-system-x86_64 \
 
 # TODO: switch to remote-viewer when a new release of virt-viewer is
 #       released.  Currently remote-viewer doesn't support keyboard
-#       grab inhibition.
-spicy --title Windows --uri="spice+unix:///tmp/vm_spice.socket"
 #       grab inhibition.  Consider using -display spice-app option.
+spicy --title Windows --uri="spice+unix://$SPICE_SOCKET"
