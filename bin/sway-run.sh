2b103695481d2c56b18d52a4391dfe3bebca299e
Davide Masserut <dm@mssdvd.com>
2021-11-02 23:39:06 +0100

    Revert "sway: fix firefox launch by XWayland applications"
    This reverts commit 948ba5fd74d57756de8d4167a04709edc5283bc2.
    
    Should have been fixed in Firefox 94.

--- bin/sway-run.sh
+++ bin/sway-run.sh
@@ -4,7 +4,6 @@ export TERM=foot
 export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
 export XDG_CURRENT_DESKTOP=sway
-export MOZ_DBUS_REMOTE=1
 export MOZ_ENABLE_WAYLAND=1
 export CLUTTER_BACKEND=wayland
 export QT_QPA_PLATFORM=wayland
