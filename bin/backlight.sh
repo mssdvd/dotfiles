43812a1900eacfa99ea84101680939f1d56e8391
Davide Masserut <dm@mssdvd.com>
2021-11-17 13:10:40 +0100

    backlight.sh: remove bashisms

--- bin/backlight.sh
+++ bin/backlight.sh
@@ -2,7 +2,7 @@
 
 output=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused == true) | .name')
 
-if [ $output == "eDP-1" ]
+if [ "$output" = "eDP-1" ]
 then exec /bin/light "$@"
 fi
 
@@ -16,18 +16,18 @@ level=$(ddcutil getvcp 10 -t | cut -d' ' -f4)
 
 case "$1" in
     -A)
-        level=$(($level + $2))
+        level=$($level + "$2")
         ;;
     -U)
-        level=$(($level - $2))
+        level=$($level - "$2")
         ;;
     -S)
-        level=$(printf "%.0f\n" $2)
+        level=$(printf "%.0f\n" "$2")
         ;;
     *)
-        echo $(ddcutil getvcp 10 -t | cut -d' ' -f4)
+        ddcutil getvcp 10 -t | cut -d' ' -f4
         exit 0
         ;;
 esac
 
-ddcutil setvcp 10 $level
+ddcutil setvcp 10 "$level"
