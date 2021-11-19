2853b4438f8488919d4e2ed64c7cb89fa10b2b88
Davide Masserut <dm@mssdvd.com>
2021-11-19 09:39:40 +0100

    emacs/consult: rename consult-imenu-project to consult-imenu-multi

--- .emacs.el
+++ .emacs.el
@@ -343,7 +343,7 @@ Intended as :after advice for `delete-file'."
          ("M-g m" . consult-mark)
          ("M-g k" . consult-global-mark)
          ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-project)
+         ("M-g I" . consult-imenu-multi)
          ;; M-s bindings (search-map)
          ("M-s g" . consult-git-grep)
          ("M-s l" . consult-line)
