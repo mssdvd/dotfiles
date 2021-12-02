42a7baf94711148f14e317132b5ab9bf3b6527e1
Davide Masserut <dm@mssdvd.com>
2021-12-02 22:27:33 +0100

    sway-run: do not run ssh-agent

--- bin/sway-run.sh
+++ bin/sway-run.sh
@@ -21,11 +21,11 @@ export XDG_MUSIC_DIR="$HOME/music"
 export XDG_PICTURES_DIR="$HOME/pictures"
 export XDG_VIDEOS_DIR="$HOME/videos"
 
-eval $(ssh-agent)
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
+# eval "$(ssh-agent)"
 
 case $1 in
 	debug)
