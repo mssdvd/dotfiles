ed3801640c5a267f1103150106132c2f4b91a9b8
Davide Masserut <dm@mssdvd.com>
2021-12-30 10:07:08 +0100

    emacs/tab-bar: add menu button to tab-bar

--- .emacs.el
+++ .emacs.el
@@ -1213,6 +1213,9 @@ Intended as :after advice for `delete-file'."
   :defer 1
   :config (winner-mode))
 
+(use-package tab-bar
+  :config (add-to-list 'tab-bar-format #'tab-bar-format-menu-bar))
+
 (use-package treemacs)
 
 (use-package treemacs-magit
