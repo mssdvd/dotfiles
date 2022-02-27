327821d8bc5012dd985cb543e08d02583a9f88ad
Davide Masserut <dm@mssdvd.com>
2022-02-27 13:21:51 +0100

    run_livecd.sh: remove bashisms

--- bin/run_livecd.sh
+++ bin/run_livecd.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-args=(
-    -machine type=q35,accel=kvm
-    -smp cores=4,threads=2
-    -m 3G
-    -nic user,model=virtio
-    -cpu host
-    -vga virtio
-    -device intel-hda
-    -device hda-duplex
-    -usb
-    -device usb-tablet
-    -display gtk,gl=on
-    -object rng-random,id=rng0,filename=/dev/urandom
-    -device virtio-rng-pci,rng=rng0
-)
-
-exec qemu-system-x86_64 "${args[@]}" -cdrom "$@"
+qemu-system-x86_64 \
+    -machine type=q35,accel=kvm \
+    -smp cores=4,threads=2 \
+    -m 3G \
+    -nic user,model=virtio \
+    -cpu host \
+    -vga virtio \
+    -device intel-hda \
+    -device hda-duplex \
+    -usb \
+    -device usb-tablet \
+    -display gtk,gl=on \
+    -object rng-random,id=rng0,filename=/dev/urandom \
+    -device virtio-rng-pci,rng=rng0 \
+    -cdrom \
+    "$@"
