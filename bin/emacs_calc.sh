fd2f20c21de0185bf766fc5011c25b19f8694ae3
Davide Masserut <dm@mssdvd.com>
2022-02-27 13:17:06 +0100

    emacs_calc.sh: connect to an emacs server instance

--- bin/emacs_calc.sh
+++ bin/emacs_calc.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-emacs --quick --basic-display --reverse-video --title="emacs calc" --funcall=full-calc \
-      --eval="(progn (setq calc-group-digits t
-                           native-comp-deferred-compilation nil
-                           native-comp-async-report-warnings-errors nil
-                           select-enable-primary t)
-                     (set-face-attribute 'default nil
-                                         :family \"Iosevka\"
-                                         :weight 'semi-bold
-                                         :height 180)
-                     (local-set-key (kbd \"q\") 'kill-emacs))"
+emacsclient -a '' -c -e "(full-calc)"
