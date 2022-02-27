5a57e957b88d0132c9e5578f7cdb36d4fdc37a5d
Davide Masserut <dm@mssdvd.com>
2022-02-27 13:19:04 +0100

    vm/windows: use remote-viewer

--- vm/win.sh
+++ vm/win.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 VM_DIR=$HOME/vm
-SPICE_SOCKET=/tmp/win_spice.socket
-MONITOR_SOCKET=/tmp/win_monitor.socket
 
 qemu-system-x86_64 \
     -daemonize \
@@ -16,7 +14,6 @@ qemu-system-x86_64 \
     -device ich9-intel-hda \
     -device hda-output,audiodev=snd0 \
     -rtc base=localtime \
-    -monitor unix:$MONITOR_SOCKET,server,nowait \
     -usb -device usb-tablet \
     -device ich9-usb-ehci1,id=usb \
     -device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on \
@@ -25,17 +22,10 @@ qemu-system-x86_64 \
     -chardev spicevmc,name=usbredir,id=usbredirchardev1 -device usb-redir,chardev=usbredirchardev1,id=usbredirdev1 \
     -chardev spicevmc,name=usbredir,id=usbredirchardev2 -device usb-redir,chardev=usbredirchardev2,id=usbredirdev2 \
     -chardev spicevmc,name=usbredir,id=usbredirchardev3 -device usb-redir,chardev=usbredirchardev3,id=usbredirdev3 \
+    -display spice-app,gl=on \
     -vga none -device qxl-vga,vgamem_mb=64 \
     -device virtio-serial-pci \
-    -spice unix=on,addr=$SPICE_SOCKET,disable-ticketing=on \
-    -device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
-    -chardev spicevmc,id=vdagent,name=vdagent \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
     -drive if=pflash,format=raw,file="$VM_DIR"/uefi_vars.fd \
     -drive driver=qcow2,file="$VM_DIR"/win11.qcow2,if=virtio,aio=native,cache.direct=on,l2-cache-size=8M \
     "$@"
-
-# TODO: switch to remote-viewer when a new release of virt-viewer is
-#       released.  Currently remote-viewer doesn't support keyboard
-#       grab inhibition.  Consider using -display spice-app option.
-spicy --title Windows --uri="spice+unix://$SPICE_SOCKET"
