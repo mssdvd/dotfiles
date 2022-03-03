f03ddc7a0f03aee17b2d27e2b047aa5773f82dde
Davide Masserut <dm@mssdvd.com>
2022-03-03 10:06:20 +0100

    play_in_mpv.sh: make it POSIX compliant and quote some variables

--- bin/play_in_mpv.sh
+++ bin/play_in_mpv.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     URL="$(wl-paste)"
 else
     URL="$(xclip -o)"
 fi
 
-exec mpv --speed=2.0 --force-window=immediate $@ -- $URL
+exec mpv --speed=2.0 --force-window=immediate "$@" -- "$URL"
