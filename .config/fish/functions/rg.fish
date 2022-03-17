5a9ef28abbd248c3d1d79247845763a10a4aec45
Davide Masserut <dm@mssdvd.com>
2022-03-17 21:49:03 +0100

    fish, ripgrep: pipe rg to less when the output is a terminal

---
+++ .config/fish/functions/rg.fish
@@ -0,0 +1,8 @@
+function rg
+    if isatty 1
+        command rg --pretty $argv \
+            | less --RAW-CONTROL-CHARS --quit-if-one-screen --no-init
+    else
+        command rg $argv
+    end
+end
