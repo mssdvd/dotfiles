e632be787111ffbd0ea93f804711d36f37970882
Davide Masserut <dm@mssdvd.com>
2022-03-25 15:22:32 +0100

    fish: quote $EDITOR

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set fish_greeting
 
 fish_add_path $HOME/{.local/bin, bin, .cargo/bin, go/bin}
 
-set -gx EDITOR emacsclient -t -a ''
+set -gx EDITOR "emacsclient -t -a ''"
 
 alias qemu qemu-system-x86_64
 alias ssh "env TERM=xterm-256color ssh"
