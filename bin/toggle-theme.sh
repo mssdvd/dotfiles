d4bbfd549f07872066c93b4b12854c409d60c17e
Davide Masserut <dm@mssdvd.com>
2022-07-01 19:01:29 +0200

    toggle-theme.sh: add support for GTK4

--- bin/toggle-theme.sh
+++ bin/toggle-theme.sh
@@ -6,12 +6,15 @@ if [ "$gtk_theme" = "'Adwaita'" ]
 then
     # Dark themes
     gtk_theme='Adwaita-dark'
+    gtk4_color_scheme='prefer-dark'
     emacs_theme='vivendi'
 else
     # Light themes
     gtk_theme='Adwaita'
+    gtk4_color_scheme='prefer-light'
     emacs_theme='operandi'
 fi
 
 gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme
+gsettings set org.gnome.desktop.interface color-scheme $gtk4_color_scheme
 emacsclient --eval "(progn (modus-themes-load-themes)(modus-themes-load-$emacs_theme))"
