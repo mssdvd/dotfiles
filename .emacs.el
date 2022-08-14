b4acb9d9adf3de20f2b34fac5dbca4dc6c15eb4e
Davide Masserut <dm@mssdvd.com>
2022-08-14 02:50:57 +0200

    emacs: remove melpa-stable repository

--- .emacs.el
+++ .emacs.el
@@ -7,7 +7,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
