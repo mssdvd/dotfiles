f112db0f70d41426bbd02ce25238f5969b699151
Davide Masserut <dm@mssdvd.com>
2023-05-02 11:09:56 +0200

    toggle-theme.sh: update modus themes toggling

--- bin/toggle-theme.sh
+++ bin/toggle-theme.sh
@@ -2,19 +2,18 @@
 
 gtk_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
-if [ "$gtk_theme" = "'Adwaita'" ]
-then
-    # Dark themes
-    gtk_theme='Adwaita-dark'
-    gtk4_color_scheme='prefer-dark'
-    emacs_theme='vivendi'
+if [ "$gtk_theme" = "'Adwaita'" ]; then
+	# Dark themes
+	gtk_theme='Adwaita-dark'
+	gtk4_color_scheme='prefer-dark'
+	emacs_theme='modus-vivendi'
 else
-    # Light themes
-    gtk_theme='Adwaita'
-    gtk4_color_scheme='prefer-light'
-    emacs_theme='operandi'
+	# Light themes
+	gtk_theme='Adwaita'
+	gtk4_color_scheme='prefer-light'
+	emacs_theme='modus-operandi'
 fi
 
 gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme
 gsettings set org.gnome.desktop.interface color-scheme $gtk4_color_scheme
-emacsclient --eval "(progn (modus-themes-load-themes)(modus-themes-load-$emacs_theme))" >/dev/null
+emacsclient --eval "(progn (require 'modus-themes)(load-theme '$emacs_theme :no-confirm))" >/dev/null
