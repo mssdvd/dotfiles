95ec660885b3971a962e7b82d1111c029d576560
Davide Masserut <dm@mssdvd.com>
2023-06-08 18:35:04 +0200

    toggle-theme.sh: use modus-themes-toggle

--- bin/toggle-theme.sh
+++ bin/toggle-theme.sh
@@ -6,14 +6,12 @@ if [ "$gtk_theme" = "'Adwaita'" ]; then
 	# Dark themes
 	gtk_theme='Adwaita-dark'
 	gtk4_color_scheme='prefer-dark'
-	emacs_theme='modus-vivendi'
 else
 	# Light themes
 	gtk_theme='Adwaita'
 	gtk4_color_scheme='prefer-light'
-	emacs_theme='modus-operandi'
 fi
 
 gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme
 gsettings set org.gnome.desktop.interface color-scheme $gtk4_color_scheme
-emacsclient --eval "(progn (require 'modus-themes)(load-theme '$emacs_theme :no-confirm))" >/dev/null
+emacsclient --eval "(modus-themes-toggle)" >/dev/null
