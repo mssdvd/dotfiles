4947e2ff33b2279563f837b56cc6fc67e1271558
Davide Masserut <dm@mssdvd.com>
2023-09-05 10:37:36 +0200

    emacs: save compile-command and register-alist across sessions

--- .emacs.el
+++ .emacs.el
@@ -26,7 +26,7 @@
  require-final-newline t
  ring-bell-function 'ignore
  save-interprogram-paste-before-kill t
- savehist-additional-variables '(kill-ring)
+ savehist-additional-variables '(compile-command kill-ring register-alist)
  scroll-error-top-bottom t
  scroll-preserve-screen-position t
  set-mark-command-repeat-pop t
