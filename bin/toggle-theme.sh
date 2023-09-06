d4c40184672357895269ee2e3b686d592c8eea7d
Davide Masserut <dm@mssdvd.com>
2023-09-06 22:47:11 +0200

    toggle-theme.sh: allow setting preferred color scheme

--- bin/toggle-theme.sh
+++ bin/toggle-theme.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-color_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
+color_scheme=${1:-$(gsettings get org.gnome.desktop.interface color-scheme)}
 
-if [ "$color_scheme" = "'prefer-light'" ]; then
+if [ "$color_scheme" = "dark" ] || [ "$color_scheme" = "'prefer-light'" ]; then
 	gtk3_theme='Adwaita-dark'
 	color_scheme='prefer-dark'
 	emacs_theme='modus-vivendi'
-else
+elif [ "$color_scheme" = "light" ] || [ "$color_scheme" = "'prefer-dark'" ]; then
 	gtk3_theme='Adwaita'
 	color_scheme='prefer-light'
 	emacs_theme='modus-operandi'
+else
+    exit 1
 fi
 
-gsettings set org.gnome.desktop.interface gtk-theme $gtk3_theme
-gsettings set org.gnome.desktop.interface color-scheme $color_scheme
+gsettings set org.gnome.desktop.interface gtk-theme "$gtk3_theme"
+gsettings set org.gnome.desktop.interface color-scheme "$color_scheme"
 emacsclient --eval "(modus-themes-load-theme '$emacs_theme)" >/dev/null
