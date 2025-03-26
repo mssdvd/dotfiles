8e9a64f10b9f8f2370ce8791747a8d8acdfa920c
Davide Masserut <dm@mssdvd.com>
2025-03-26 20:37:25 +0100

    ssh: start ssh-agent with systemd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,17 +4,11 @@ fish_add_path $HOME/{.local/bin, bin, .cargo/bin, go/bin}
 
 set -gx EDITOR "emacsclient -t -a ''"
 set -gx MAKEFLAGS "-j$(nproc)"
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 alias qemu qemu-system-x86_64
 alias ssh "env TERM=xterm-256color ssh"
 alias yt yt-dlp
-
-if ! pgrep -u "$USER" ssh-agent > /dev/null
-    ssh-agent -c -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
-end
-if ! set -q SSH_AUTH_SOCK
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
-end
 
 if status is-login; and test -z "$DISPLAY"; and test $(tty) = "/dev/tty1"
     exec sway-run.sh
--- bin/sway-run.sh
+++ bin/sway-run.sh
@@ -16,6 +16,4 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
-# eval "$(ssh-agent)"
-
 exec systemd-cat --identifier=sway sway
