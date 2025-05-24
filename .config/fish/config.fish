a7e29d7828a6eb9e87b71b619a164ff5ddc892e6
Davide Masserut <dm@mssdvd.com>
2025-05-24 11:43:08 +0200

    fish/less: enable smart case-sensivity

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ fish_add_path $HOME/{.local/bin, bin, .cargo/bin, go/bin}
 set -gx EDITOR "emacsclient -t -a ''"
 set -gx MAKEFLAGS "-j$(nproc)"
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -gx LESS "-i"
 
 alias qemu qemu-system-x86_64
 alias ssh "env TERM=xterm-256color ssh"
