e8aa6c90c66bff9da3168b5d2018ebe16ef2b963
Davide Masserut <dm@mssdvd.com>
2025-10-14 23:06:52 +0200

    sway-run.sh: use the executable name to describe the log source

--- bin/sway-run.sh
+++ bin/sway-run.sh
@@ -16,4 +16,4 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
-exec systemd-cat --identifier=sway sway
+exec systemd-cat sway
