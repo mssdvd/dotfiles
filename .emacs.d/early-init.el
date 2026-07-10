2e804b29be365f1cd12a29cb184b406a4da6b101
Davide Masserut <dm@mssdvd.com>
2026-07-10 13:52:25 +0200

    emacs: tune startup GC/frame size, add eshell read-only aliases, split if template

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,14 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
+;; If Emacs is built with the new MPS-based garbage collector, these variables have no effect
 ;; Increase gc thresholds during startup
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
+(setq gc-cons-threshold (expt 2 30)
+      gc-cons-percentage 0.6
+      )
 
 ;; Reset gc thresholds
-(add-hook 'emacs-startup-hook (lambda ()
-                                (setq gc-cons-threshold 1600000
-                                      gc-cons-percentage 0.1)))
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 800000 4)
+                                               gc-cons-percentage 0.2)))
+
+(setq default-frame-alist '((width . 120)
+                            (height . 60)))
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
