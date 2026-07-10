c583e7139895b35d2ddbedee9857b7fb93d94d3b
Davide Masserut <dm@mssdvd.com>
2026-07-10 12:34:05 +0200

    shell: refresh fish/bash env, readline completion behavior
    Add fzf keybindings, ledger/debuginfod env vars, and a !! history
    abbreviation in fish; tune inputrc for menu-complete and prefix-based
    history search.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,20 @@
 set fish_greeting
 
-set -gx EDITOR "emacsclient -t -a ''"
+set -gx BROWSER "firefox"
+set -gx DEBUGINFOD_URLS "https://debuginfod.archlinux.org/"
+set -gx EDITOR "emacsclient -t --alternate-editor="
 set -gx MAKEFLAGS "-j$(nproc)"
+set -gx LEDGER_FILE "$HOME/ledger/ledger.ldg"
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-set -gx LESS "-i"
+# set -gx LESS "-i"
+
+function last_history_item; echo $history[1]; end
+abbr -a !! --position anywhere --function last_history_item
 
 alias qemu qemu-system-x86_64
 alias ssh "env TERM=xterm-256color ssh"
 alias yt yt-dlp
+alias ll "ls -lh --color=auto"
 
+fzf --fish | FZF_CTRL_T_COMMAND= source
+bind \ex fzf-file-widget
